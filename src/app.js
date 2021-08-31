e832e0414a49919955df060d5e7c27137cd4b5a6
Taigah <taigah.dev@gmail.com>
2021-08-31 15:27:36 +0200

    switch to preact-custom-element

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import { ReactiveButton } from "./components/ReactiveButton.js"
 import { ReactiveButtonBatch } from './components/ReactiveButtonBatch.js'
 import { preactElementDefine } from './preactElementDefine.js'
 
-preactElementDefine("reactive-button", ReactiveButton, [ "initial-value" ])
-preactElementDefine("reactive-button-batch", ReactiveButtonBatch, [ "initial-value", "count" ])
+preactElementDefine("reactive-button", ReactiveButton)
+preactElementDefine("reactive-button-batch", ReactiveButtonBatch)
--- src/preactElementDefine.js
+++ src/preactElementDefine.js
@@ -1,16 +1,5 @@
-import { h, render } from 'preact'
+import register from 'preact-custom-element'
 
-export function preactElementDefine(name, component, allowedProps = []) {
-  customElements.define(name, class extends HTMLElement {
-    connectedCallback() {
-      const props = {}
-      for (const prop of allowedProps) {
-        if (this.hasAttribute(prop)) {
-          props[prop] = this.getAttribute(prop)
-        }
-      }
-      const children = []
-      render(h(component, props, children), this)
-    }
-  })
+export function preactElementDefine(name, component) {
+  register(component, name)
 }
